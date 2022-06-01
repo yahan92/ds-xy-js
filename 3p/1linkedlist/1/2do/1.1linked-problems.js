b87ece4f30b192e06ec7c6cc852388ff4e6a6079
yahan92 <101825250+yahan92@users.noreply.github.com>
2022-06-01 15:50:45 -0500

    Update 1.1linked-problems.js

--- 3p/1linkedlist/1/2do/1.1linked-problems.js
+++ 3p/1linkedlist/1/2do/1.1linked-problems.js
@@ -9,8 +9,8 @@
  * contains(v)
  *
  * look up for value v in the linked list
- * @arg {type} v - value; may be of a char, number, u object
- * @returns {bool} - true or false, if value v exist or not
+ * @arg {type} v - value; may be of chars, numbers, u objects
+ * @returns {bool} - true of false, if value v exist or not
  * may use traverse
  *
  * @example
@@ -19,3 +19,64 @@
  * @see {@link https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array}
  * @see {@link https://jsdoc.app}
  */
+
+class LinkedList {
+  constructor(head = null) {
+    this.head = head;
+  }
+
+  size() {
+    let count = 0;
+    let node = this.head;
+    while (node) {
+      count++;
+      node = node.next;
+    }
+    return count;
+  }
+
+  clear() {
+    this.head = null;
+  }
+
+  contains(v) {
+    let node = this.head;
+    while (node != null && node.data != v) node = node.next;
+    return node ? true : false;
+  }
+
+  getLast() {
+    let lastNode = this.head;
+    if (lastNode) {
+      while (lastNode.next) {
+        lastNode = lastNode.next;
+      }
+    }
+    return lastNode;
+  }
+
+  getFirst() {
+    return this.head;
+  }
+
+  insert(node) {
+    this.getLast().next = node;
+  }
+}
+
+class ListNode {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+let head = new ListNode(2);
+let node1 = new ListNode(5);
+head.next = node1;
+let node2 = new ListNode("a");
+node1.next = node2;
+
+let list = new LinkedList(head);
+
+console.log(list.contains("a"));
