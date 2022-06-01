bf592affa189b9ecadc838b6afe31a088d85af4b
yahan92 <101825250+yahan92@users.noreply.github.com>
2022-06-01 16:10:11 -0500

    Update 2.2tree-problems.js

--- 3p/2trees/linked-/2/2do/2.2tree-problems.js
+++ 3p/2trees/linked-/2/2do/2.2tree-problems.js
@@ -25,3 +25,78 @@
  * posorder(root)
  * 
  */
+
+
+
+class Node {
+  constructor(data, left, right) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
+
+  show() {
+    return this.data;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+  insert(data) {
+    var n = new Node(data, null, null);
+    if (this.root == null) {
+      this.root = n;
+    } else {
+      var current = this.root;
+      var parent;
+      while (true) {
+        parent = current;
+        if (data < current.data) {
+          current = current.left;
+          if (current == null) {
+            parent.left = n;
+            break;
+          }
+        } else {
+          current = current.right;
+          if (current == null) {
+            parent.right = n;
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  inOrder(node = this.root) {
+    if (!(node == null)) {
+      this.inOrder(node.left);
+      console.log(node.show() + " ");
+      this.inOrder(node.right);
+    }
+  }
+
+  postOrder(node = this.root) {
+    if (!(node == null)) {
+      this.postOrder(node.left);
+      this.postOrder(node.right);
+      console.log(node.show() + " ");
+    }
+  }
+}
+
+var nums = new BST();
+
+nums.insert(31);
+nums.insert(9);
+nums.insert(56);
+nums.insert(48);
+nums.insert(77);
+nums.insert(12);
+nums.insert(69);
+console.log("Inorder traversal: ");
+nums.inOrder(nums.root);
+console.log("Postorder traversal: ");
+nums.postOrder(nums.root);
