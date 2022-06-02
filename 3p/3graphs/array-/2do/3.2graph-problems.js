782f9d914fc6c14a054250aed9e3defd70ca1b30
yahan92 <101825250+yahan92@users.noreply.github.com>
2022-06-01 20:54:04 -0500

    Update 3.2graph-problems.js

--- 3p/3graphs/array-/2do/3.2graph-problems.js
+++ 3p/3graphs/array-/2do/3.2graph-problems.js
@@ -1,10 +1,10 @@
 /** INSTRUCTIONS
- * 
+ *
  * create one file by program
  * implement a method, function, non-function solution
  * solve each as requested
  * may use code from previous exercises
- * 
+ *
  * PROBLEM 1
  * isSquare(g)
  *
@@ -14,7 +14,7 @@
  *
  * @calling example
  * isSquare(g)
- * 
+ *
  * PROBLEM 2
  * isSymmetrical(g)
  *
@@ -49,3 +49,86 @@
  * @see {@link https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array}
  * @see {@link https://jsdoc.app}
  */
+
+let directed =  [
+    [0, 1, 0, 1, 0, 0],
+    [0, 0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 1, 1],
+    [0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0, 1],
+]
+let undirected =  [
+    [0, 1, 0, 0, 1],
+    [1, 0, 1, 1, 1],
+    [0, 1, 0, 1, 0],
+    [0, 1, 1, 0, 1],
+    [1, 1, 0, 1, 0],
+]
+let tree =  [
+    [0, 1, 0, 0],//1
+    [1, 0, 1, 1],//2
+    [0, 1, 0, 0],//3
+    [0, 1, 0, 0],//4
+]
+
+const isSquire = (g) => {
+    let flag=true
+    for(let i=0; i <= g.length-1; i++){
+        for(let j=0; j <= g[i].length-1; j++){
+        }
+        if(g.length-1 != g[i].length-1){
+            flag=false
+        }
+
+    }
+    return flag
+}
+
+console.log('is squire: ' + isSquire(directed))
+
+const isSymmetrical = (g) => {
+    let flag=true
+    for(let i=0; i <= g.length-1; i++){
+        for(let j=0; j <= g.length-1; j++){
+           if(g[i][j] != g[j][i]){
+               flag=false
+           }
+        }
+    }
+    return flag
+}
+
+console.log('is symmetrical: ' + isSymmetrical(undirected))
+
+const isTree = (t) => {
+    let count = 0;
+    let column = 0;
+    let s = 0;
+    let flag=true
+
+    for (let i = 0; i <= t.length-1; i++) {
+        for (let j = 0; j < t[i].length; j++, count++)
+            column = count / t.length;
+    }
+    for (let i = 1; i <= t.length-1; i++) {
+        s = 0;
+        for (let j = 0; j < column; j++) {
+            if (j < i) {
+                s += t[i][j];
+                if (s > 1) flag = false;
+            }
+        }
+    }
+    return flag;
+}
+
+console.log('is a tree: '+isTree(tree))
+
+const isGraph = (g) => {
+        let flag = true
+        if (isTree(g) == true) flag = false
+        return flag
+}
+
+console.log('is a graph: '+ isGraph(tree))
